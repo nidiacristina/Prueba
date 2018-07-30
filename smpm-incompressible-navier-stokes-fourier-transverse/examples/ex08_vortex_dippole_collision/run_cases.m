06758e9ca4a5f685c3ebb7328c7875b275f901b0
nidiacristina <nr438@cornell.edu>
2018-07-30 09:46:21 -0400

    With routine to test different grids in example 08-2

--- smpm-incompressible-navier-stokes-fourier-transverse/examples/ex08_vortex_dippole_collision/run_cases.m
+++ smpm-incompressible-navier-stokes-fourier-transverse/examples/ex08_vortex_dippole_collision/run_cases.m
@@ -22,8 +22,8 @@ for k=1:length(p_n)
         nsubx=p_nsubx(j);
         nsubz=nsubx;
         %Remove files resulting from the previous simulations
-        %delete *vortex_dippole_collision_out_0000
-        %delete *vortex_dippole_collision_restart_0000
+        delete *vortex_dippole_collision_out_0000
+        delete *vortex_dippole_collision_restart_0000
         %Builds the inputs for the case
 
         %%%Crear input
@@ -34,44 +34,46 @@ for k=1:length(p_n)
         [status,cmdout] = system(command,'-echo')
         status
 
-        %%%%Leer resultados
+        try
+            %%%%Leer resultados
 
-        fileList = dir('*out*.h5');
-        ent=zeros(length(fileList),1);
-        ens=zeros(length(fileList),1);
-        pal=zeros(length(fileList),1);
-        ttime=zeros(length(fileList),1);
+            fileList = dir('*out*.h5');
+            ent=zeros(length(fileList),1);
+            ens=zeros(length(fileList),1);
+            pal=zeros(length(fileList),1);
+            ttime=zeros(length(fileList),1);
 
-        for i=1:length(fileList)
-            data = smpm_read_fieldfile(fileList(i).name,'NEW');
-            ttime(i)= data.field.time;
-            ent(i)= compute_kinetic_energy(data);
-            ens(i)= compute_enstrophy(data);
-            pal(i)= compute_palinstrophy(data);
-        end
+            for i=1:length(fileList)
+                data = smpm_read_fieldfile(fileList(i).name,'NEW');
+                ttime(i)= data.field.time;
+                ent(i)= compute_kinetic_energy(data);
+                ens(i)= compute_enstrophy(data);
+                pal(i)= compute_palinstrophy(data);
+            end
 
-        save(['NR_2500_Variables_N_',num2str(n,2),'_Mx_',num2str(nsubx,2),'_Mz_',num2str(nsubz,2),'.mat'],'ent','ens','ttime')
+            save(['NR_2500_Variables_N_',num2str(n,2),'_Mx_',num2str(nsubx,2),'_Mz_',num2str(nsubz,2),'.mat'],'ent','ens','ttime')
 
-        figure
-        plot(ttime,ent,'b')
-        xlabel('t')
-        ylabel('E (t)')
-        saveas(gcf,['NR_2500_KineticEnergy_N_',num2str(n,2),'_Mx_',num2str(nsubx,2),'_Mz_',num2str(nsubz,2),'.png'])
+            figure
+            plot(ttime,ent,'b')
+            xlabel('t')
+            ylabel('E (t)')
+            saveas(gcf,['NR_2500_KineticEnergy_N_',num2str(n,2),'_Mx_',num2str(nsubx,2),'_Mz_',num2str(nsubz,2),'.png'])
 
-        figure
-        plot(ttime,ens,'g')
-        xlabel('t')
-        ylabel('\Omega (t)')
-        saveas(gcf,['NR_2500_Enstrophy_N_',num2str(n,2),'_Mx_',num2str(nsubx,2),'_Mz_',num2str(nsubz,2),'.png'])
+            figure
+            plot(ttime,ens,'g')
+            xlabel('t')
+            ylabel('\Omega (t)')
+            saveas(gcf,['NR_2500_Enstrophy_N_',num2str(n,2),'_Mx_',num2str(nsubx,2),'_Mz_',num2str(nsubz,2),'.png'])
 
-        figure
-        plot(ttime,pal,'r')
-        xlabel('t')
-        ylabel('\P (t)')
-        saveas(gcf,['NR_2500_Palinstrophy_N_',num2str(n,2),'_Mx_',num2str(nsubx,2),'_Mz_',num2str(nsubz,2),'.png'])
-
+            figure
+            plot(ttime,pal,'r')
+            xlabel('t')
+            ylabel('\P (t)')
+            saveas(gcf,['NR_2500_Palinstrophy_N_',num2str(n,2),'_Mx_',num2str(nsubx,2),'_Mz_',num2str(nsubz,2),'.png'])
+            close all
         
-        close all
-
+        catch
+            
+        end
     end
  end
